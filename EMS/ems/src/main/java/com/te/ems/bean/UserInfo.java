2fc5ff8c004bf2b786924fe0e98fe019550b161e
Aravind <aravindhankumar97@gmail.com>
2022-01-05 10:05:42 +0530

    Bean class was added

---
+++ EMS/ems/src/main/java/com/te/ems/bean/UserInfo.java
@@ -0,0 +1,38 @@
+package com.te.ems.bean;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="user_info")
+public class UserInfo implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	
+	@Id
+	@Column(name="user_name")
+	private String userName;
+	
+	private Integer age;
+	
+	private String job;
+	
+	private Double salary;
+	
+	private String mail;
+	
+	private String password;
+	
+}
